a04e97e4994013176695854f2d2643922f3bccca
vsguts <vsguts@gmail.com>
2019-04-01 20:27:48 +0300

    DB logic was implemented.

---
+++ config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'mysql',
+        'port' => '3306',
+        'username' => 'catalog',
+        'password' => 'catalog',
+        'db_name' => 'catalog',
+    ],
+];
