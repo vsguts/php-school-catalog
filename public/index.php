1fa4ff6458f30fa6f11922c25f4e40b1772642f5
Ilya <ilyamakey@gmail.com>
2019-04-03 11:11:39 +0300

    change controllers actions from echo to return statement

--- public/index.php
+++ public/index.php
@@ -10,4 +10,9 @@ $router = require __DIR__ . '/../routes/routes.php';
 $app = new Application($router);
 
 $request = new Request;
-$app->handRequest($request);
+
+echo $app->handRequest($request);
+
+
+//- Нужно сделать так, чтобы контроллеры не выводили результат на экран, а возвращали этот результат.
+//- А в index.php надо поставить echo перед $app->handleRequest($request); чтобы результат вывелся на экран.
