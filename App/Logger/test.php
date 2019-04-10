2826978ae2d39785e7140b4884cf5e5d7bbc11be
LiraShillien <46996728+LiraShillien@users.noreply.github.com>
2019-04-10 16:41:22 +0300

    Update test.php

--- App/Logger/test.php
+++ App/Logger/test.php
@@ -1,15 +1,2 @@
 <?php
 
-//use App\Logger\Logger;
-
-
-
-
-try {
-    $x = 4;
-    if ($x !== 5) {
-        throw new Exception('Route not found');
-    }
-}catch (Exception $e) {
-    echo 'ldldldldldldld';
-}
