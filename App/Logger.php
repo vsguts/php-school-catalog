d61e3285a3d08597d5e06a43dac9ca35a592a791
Denis <Denis K>
2019-04-14 21:58:00 +0300

    Logger

--- App/Logger.php
+++ App/Logger.php
@@ -4,16 +4,16 @@ namespace App;
 
 class Logger implements LoggerInterface
 {
+    private $folder = '../logs/';
+
     public function log($message, $errorType = "INFO")
     {
-        $folder = '../logs/';
-
-        if (!file_exists($folder) && !is_dir($folder)) {
-            mkdir($folder);
+        if (!file_exists($this->folder) && !is_dir($this->folder)) {
+            mkdir($this->folder);
         }
 
 
-        file_put_contents($folder . date('Y_m_d_H') . '.log',
+        file_put_contents($this->folder . date('Y_m_d_H') . '.log',
             sprintf("[%s] Level: `%s`. Message: `%s`", date('Y-m-d H:i:s'), $errorType, $message) . PHP_EOL,
             FILE_APPEND);
     }
