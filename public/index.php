44281de981d0ffc6b410978db1d73b0bcad05519
Denis <denis@Deniss-MacBook-Pro.local>
2019-06-30 20:26:00 +0300

    Update

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 use App\Application;
 use App\Http\Request;
 
-require __DIR__ . '/../bootstrap/bootstrap.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 $router = require __DIR__ . '/../routes/routes.php';
 
@@ -11,3 +11,11 @@ $app = new Application($router);
 
 $request = new Request;
 $app->handleRequest($request);
+
+$phinxApp = new \Phinx\Console\PhinxApplication();
+$phinxTextWrapper = new \Phinx\Wrapper\TextWrapper($phinxApp);
+
+$phinxTextWrapper->setOption('configuration', '/../phinx.yml');
+$phinxTextWrapper->setOption('parser', 'YAML');
+$phinxTextWrapper->setOption('environment', 'development');
+$phinxTextWrapper->getMigrate();
